84f67c993542b0f23367efa98fe51d9d916cdc9d
Zach <tuj54380@temple.edu>
2022-11-29 15:19:07 -0500

    Finished

--- app/src/main/java/com/example/actuallyanything/MainActivity.kt
+++ app/src/main/java/com/example/actuallyanything/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,12 +24,20 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.TakePicture -> Toast.makeText(this, "Taking Picture", Toast.LENGTH_SHORT).show()
-            R.id.Delete -> {}
+            R.id.Delete -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Confirmation")
+                    .setMessage("Are you sure you would like to delete this picture? This action cannot be undone.")
+                    .setPositiveButton("Yes"){_,_ -> Toast.makeText(this, "Deleting Picture", Toast.LENGTH_SHORT).show()}
+                    .setNegativeButton("Never Mind"){dialog,_ -> dialog.cancel()}
+                    .setCancelable(false)
+                    .create()
+                    .show()
+            }
             R.id.Help -> startActivity(Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.temple.edu")))
 
             else -> return false
         }
-
         return true
     }
 }
